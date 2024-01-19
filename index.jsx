93332f686269244f41d6a36cae9ca2ffae2c494e
Harc28 <157147859+Harc28@users.noreply.github.com>
2024-01-20 05:29:38 +0530

    Create index.jsx

---
+++ index.jsx
@@ -0,0 +1,237 @@
+import React from "react";
+import { Header } from "./Header";
+import "./style.css";
+
+export const Wireframe = () => {
+  return (
+    <div className="wireframe">
+      <div className="BG-wrapper">
+        <div className="BG">
+          <div className="overlap-2">
+            <img className="bg" alt="Bg" src="bg.svg" />
+            <img className="image" alt="Image" src="image-8.png" />
+            <Header
+              arrowDropDown="arrow-drop-down-4.svg"
+              arrowDropDown1="arrow-drop-down-7.svg"
+              arrowDropDown2="arrow-drop-down-6.svg"
+              className="header-instance"
+              genify="genify-1-2.png"
+              header="header-2.png"
+              img="arrow-drop-down-5.svg"
+              overlapClassName="design-component-instance-node"
+            />
+            <div className="text-wrapper-4">Cheapest Deals</div>
+            <img className="h-up" alt="H up" src="h-up.svg" />
+            <div className="pro">
+              <div className="overlap-3">
+                <div className="text-wrapper-5">₹6,799</div>
+                <div className="text-wrapper-6">₹6,789</div>
+                <img className="flipkart" alt="Flipkart" src="flipkart.png" />
+                <a
+                  className="group"
+                  href="https://www.amazon.in/Redmi-Aqua-Blue-64GB-Storage/dp/B0C74LZSQB/ref=sr_1_5?crid=3SCXIZ0BEC6PW&amp;keywords=cheapest+phone&amp;qid=1705697851&amp;sprefix=cheapest+phon%2Caps%2C255&amp;sr=8-5"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  <div className="overlap-group-2">
+                    <div className="text-wrapper-7">BUY NOW</div>
+                  </div>
+                </a>
+                <p className="p">Redmi A2 (Aqua Blue, 4GB RAM, 64GB Storage)</p>
+                <img className="amazon" alt="Amazon" src="amazon.png" />
+                <a
+                  className="group-2"
+                  href="https://www.flipkart.com/redmi-a2-aqua-blue-64-gb/p/itm63a05c64d910d?pid=MOBGPVYMG9RWX6XY&amp;lid=LSTMOBGPVYMG9RWX6XYCJ14RT&amp;marketplace=FLIPKART&amp;q=Redmi%20A2%20(Aqua%20Blue,%202GB%20RAM,%2064GB%20Storage)&amp;sattr[]=color&amp;sattr[]=ram&amp;st=ram"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  <div className="overlap-group-2">
+                    <div className="text-wrapper-7">BUY NOW</div>
+                  </div>
+                </a>
+                <img className="redmi" alt="Redmi" src="redmi-a2-1.png" />
+                <div className="product" />
+              </div>
+            </div>
+            <div className="pro-2">
+              <div className="overlap-4">
+                <div className="text-wrapper-8">₹27,990</div>
+                <a
+                  className="group-3"
+                  href="https://www.amazon.in/Realme-Denim-Black-32GB-Storage/dp/B0B6C8XMSS/ref=sr_1_1?crid=2JESWPKIB082S&amp;keywords=realme+C30+%283GB+RAM%2C+32GB%2C+Denim+Black%29&amp;qid=1705701178&amp;s=electronics&amp;sprefix=realme+c30+3gb+ram%2C+32gb%2C+denim+black+%2Celectronics%2C291&amp;sr=1-1"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  <div className="overlap-group-2">
+                    <div className="text-wrapper-7">BUY NOW</div>
+                  </div>
+                </a>
+                <a
+                  className="group-4"
+                  href="https://www.flipkart.com/realme-c30-denim-black-32-gb/p/itmfcf69b2db5059?pid=MOBGF37GMBMYRFBK&amp;lid=LSTMOBGF37GMBMYRFBKEDIG8L&amp;marketplace=FLIPKART&amp;q=realme+C30+%283GB+RAM%2C+32GB%2C+Denim+Black%29&amp;store=tyy%2F4io&amp;srno=s_1_3&amp;otracker=search&amp;otracker1=search&amp;fm=organic&amp;iid=cb343ced-8c72-40fa-a545-935d32093c53.MOBGF37GMBMYRFBK.SEARCH&amp;ppt=pp&amp;ppn=pp&amp;ssid=y7fdarojj40000001705701030100&amp;qH=0e791bd934a82efe"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  <div className="overlap-group-2">
+                    <div className="text-wrapper-7">BUY NOW</div>
+                  </div>
+                </a>
+                <div className="product-2" />
+                <img className="flipkart-2" alt="Flipkart" src="image.png" />
+                <p className="text-wrapper-9">Realme 11 Pro+ 5G (Sunrise Beige, 12GB RAM, 256GB Storage)</p>
+                <img className="image-2" alt="Image" src="image-1.png" />
+                <img className="amazon-2" alt="Amazon" src="amazon-2.png" />
+              </div>
+            </div>
+            <div className="pro-3">
+              <div className="overlap-4">
+                <div className="text-wrapper-8">₹499</div>
+                <a className="group-5" href="https://amzn.eu/d/euvBfR6" rel="noopener noreferrer" target="_blank">
+                  <div className="overlap-group-2">
+                    <div className="text-wrapper-7">BUY NOW</div>
+                  </div>
+                </a>
+                <a
+                  className="group-6"
+                  href="http://www.myntra.com/mailers/topwear/semir/semir-women-solid-slim-fit-t-shirt/23260308/buy"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  <div className="overlap-group-2">
+                    <div className="text-wrapper-7">BUY NOW</div>
+                  </div>
+                </a>
+                <a
+                  className="group-7"
+                  href="https://www.meesho.com/s/p/565g81?utm_source=s_w"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  <div className="overlap-group-2">
+                    <div className="text-wrapper-7">BUY NOW</div>
+                  </div>
+                </a>
+                <div className="product-2" />
+                <img className="flipkart-2" alt="Flipkart" src="flipkart-2.png" />
+                <p className="THE-TRIPPY-STORE">
+                  THE TRIPPY STORE Unisex Cotton Regular Fit Half Sleeve Round Neck Casual T-Shirt &amp; Tees (Black)
+                </p>
+                <img className="amazon-2" alt="Amazon" src="amazon-3.png" />
+              </div>
+            </div>
+            <div className="pro-4">
+              <div className="overlap-5">
+                <div className="text-wrapper-10">₹747</div>
+                <a
+                  className="group-8"
+                  href="https://www.tatacliq.com/rock-it-green-round-neck-t-shirt/p-mp000000013473607?cid=af:homepage:admitad:hasoffers:15032017"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  <div className="overlap-group-2">
+                    <div className="text-wrapper-7">BUY NOW</div>
+                  </div>
+                </a>
+                <a
+                  className="group-9"
+                  href="https://dl.flipkart.com/s/jbBuAwuuuN"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  <div className="overlap-group-2">
+                    <div className="text-wrapper-7">BUY NOW</div>
+                  </div>
+                </a>
+                <a
+                  className="group-10"
+                  href="https://www.ajio.com/rock-it-graphic-print-slim-fit-t-shirt/p/464575998_green?utm_campaign=20240120clwy8h92vv5j&amp;utm_medium=affiliate&amp;utm_source=cuelinks"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  <div className="overlap-group-2">
+                    <div className="text-wrapper-7">BUY NOW</div>
+                  </div>
+                </a>
+                <div className="product-2" />
+                <img className="flipkart-3" alt="Flipkart" src="flipkart-3.png" />
+                <p className="text-wrapper-11">Rock.it Green Round Neck T-Shirt</p>
+                <img className="amazon-3" alt="Amazon" src="amazon-4.png" />
+              </div>
+              <div className="text-wrapper-12">₹322</div>
+            </div>
+            <div className="pro-5">
+              <div className="overlap-6">
+                <div className="text-wrapper-8">₹19,495</div>
+                <a className="group-3" href="https://amzn.eu/d/gHwLh6w" rel="noopener noreferrer" target="_blank">
+                  <div className="overlap-group-2">
+                    <div className="text-wrapper-7">BUY NOW</div>
+                  </div>
+                </a>
+                <a
+                  className="group-4"
+                  href="https://dl.flipkart.com/s/j3uAs1uuuN"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  <div className="overlap-group-2">
+                    <div className="text-wrapper-7">BUY NOW</div>
+                  </div>
+                </a>
+                <div className="product-2" />
+                <img className="flipkart-2" alt="Flipkart" src="flipkart-4.png" />
+                <p className="diesel-analog-blue">Diesel Analog Blue Dial (Big Dial) Men&#39;s Watch-DZ4329I</p>
+                <img className="amazon-2" alt="Amazon" src="amazon-5.png" />
+              </div>
+              <img className="green-niggah" alt="Green niggah" src="green-niggah.png" />
+            </div>
+            <div className="pro-6">
+              <div className="overlap-7">
+                <a className="group-3" href="https://amzn.eu/d/3lmekWg" rel="noopener noreferrer" target="_blank">
+                  <div className="overlap-group-2">
+                    <div className="text-wrapper-7">BUY NOW</div>
+                  </div>
+                </a>
+                <a
+                  className="group-4"
+                  href="https://dl.flipkart.com/s/Ms0DdrNNNN"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  <div className="overlap-group-2">
+                    <div className="text-wrapper-7">BUY NOW</div>
+                  </div>
+                </a>
+                <div className="product-2" />
+                <p className="BI-FASHION-men-s">BI FASHION Men&#39;s Cotton Hooded Neck Pullover</p>
+                <img className="flipkart-2" alt="Flipkart" src="flipkart-5.png" />
+                <div className="text-wrapper-13">₹379</div>
+                <img className="amazon-2" alt="Amazon" src="amazon-6.png" />
+                <img className="blue-hoodie" alt="Blue hoodie" src="blue-hoodie.png" />
+              </div>
+              <div className="text-wrapper-14">₹21,495</div>
+              <div className="text-wrapper-15">₹747</div>
+              <div className="text-wrapper-16">₹714</div>
+              <img className="image-3" alt="Image" src="image-2.png" />
+              <div className="text-wrapper-17">₹748</div>
+              <div className="text-wrapper-18">₹155</div>
+              <img className="ajio" alt="Ajio" src="ajio.png" />
+              <img className="black-shirt" alt="Black shirt" src="black-shirt.png" />
+              <div className="text-wrapper-19">₹27,999</div>
+            </div>
+            <img className="sidebar" alt="Sidebar" src="sidebar.png" />
+            <img className="vector" alt="Vector" src="vector.svg" />
+            <img className="account-circle" alt="Account circle" src="account-circle.svg" />
+            <img className="bell" alt="Bell" src="bell.png" />
+            <div className="more">
+              <div className="overlap-8">
+                <img className="horizontal-line" alt="Horizontal line" src="horizontal-line-2.png" />
+                <img className="horizontal-line-2" alt="Horizontal line" src="horizontal-line-3.png" />
+                <img className="horizontal-line-3" alt="Horizontal line" src="horizontal-line.png" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
